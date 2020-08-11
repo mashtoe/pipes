dfffe3971daff3d692de3ee277025766a37bbf77
mashtoe-dt <mashtoe@hotmail.com>
2020-08-11 18:49:33 +0200

    no colors

---
+++ Assets/Pipe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+	private int maxLength;
+	private int additionalSameDirPipesInPool;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public IEnumerator PipeRoutine(){
+		yield return new WaitForSeconds (3f);
+	}
+}
